5741e176c1cf0db48cecd95d53c869884b4ef988
Arsalaan-Alam <studyfreak1234@gmail.com>
2023-07-01 11:33:49 +0530

    added form

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   <button class="bg-transparent hover:bg-blue-500 text-blue-600 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+   <button class="bg-transparent hover:bg-blue-500 text-blue-600 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ">
    Connect Wallet
 </button>
 
