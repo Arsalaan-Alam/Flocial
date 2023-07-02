517d0dc4be39e13784617fed98dab35625afa86c
Arsalaan-Alam <studyfreak1234@gmail.com>
2023-07-02 19:15:47 +0530

    added navbar and reconfigured fonts

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,15 @@
 import './globals.css'
+import { Montserrat } from 'next/font/google'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from './navbar'
+
+const montserrat = Montserrat({
+  weight: ['400','500','600','700','800'],
+  subsets: ['latin'],
+})
+
+ 
 
 export const metadata = {
   title: 'Flocial',
@@ -10,8 +18,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    
+    <>
+      <Navbar />
+      <body className={montserrat.className}>{children}</body>
+    </>
   )
 }
--- app/page.js
+++ app/page.js
@@ -3,12 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-   <Link href="/signup"><button class="bg-transparent hover:bg-blue-500 text-blue-600 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ">
-   Connect Wallet
-</button>
-</Link>
-
-    </main>
+    <div className="flex items-center justify-center p-24">
+      <Link href="/signup"><button class="bg-transparent hover:bg-blue-500 text-blue-600 hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded font-bold ">
+        Connect Wallet
+        </button>
+      </Link>
+    </div>
+  
+    
   )
 }
