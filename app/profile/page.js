bbc6f3eeb2b53d3ce2b515c9ebdaa543c5ab9fb5
Arsalaan-Alam <studyfreak1234@gmail.com>
2023-07-04 21:08:33 +0530

    profile page

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,12 +1,16 @@
 export default function ProfilePage(){
     return(
         <div className="flex items-center justify-center mt-20">
-            <div className="font-bold rounded-lg border shadow-xl bg-base-100 min-w-3/4 p-10 bg-grey-100">
-            <div className="grid grid-cols-10 gap-4">
-  <div className="bg-blue-100 col-span-3 p-2">1st col</div>
-  <div className="bg-red-100 col-span-7">2nd col</div>
-</div>
-
+            <div className="font-bold rounded-lg border shadow-xl bg-base-100 min-w-3/4 p-10 bg-grey-100 max-w-2xl">
+                <div className="grid grid-cols-10 gap-4">
+                    <div className="bg-blue-100 col-span-3 p-2 flex items-center justify-center">
+                        <img src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="profileimg" className=""/>
+                        
+                    </div>
+                    <div className="bg-red-100 col-span-7">
+                        2nd col
+                    </div>
+                </div>
             </div>
         </div>
  
